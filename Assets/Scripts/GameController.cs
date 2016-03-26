ed839f28d9b8ba7d8f886c55052fd661d25d940e
handrin <handrin@uwo.ca>
2016-03-26 16:00:19 -0400

    Add waves of snowflakes on title screen

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector2 spawnValues;
+    public float hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    void Start()
+    {
+        StartCoroutine(spawnWaves());
+    }
+
+    IEnumerator spawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true) {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Quaternion spawnRotation = Quaternion.identity;
+                Vector2 spawnPosition = new Vector2(spawnValues.x, (Random.Range(-spawnValues.y, spawnValues.y)));
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                spawnPosition = new Vector2(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y);
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
