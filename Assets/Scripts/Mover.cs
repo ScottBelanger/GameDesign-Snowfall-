b29baab60180f7a39f568ac09d2d1b499e3e6485
handrin <handrin@uwo.ca>
2016-03-25 21:57:16 -0400

    Add velocity to snowflakes on main menu

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalSpeed, verticalSpeed);
+    }
+}
