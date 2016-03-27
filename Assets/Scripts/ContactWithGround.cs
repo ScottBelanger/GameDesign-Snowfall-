c068358850300fee5da1d2d37b9a95dd365e045c
handrin <handrin@uwo.ca>
2016-03-27 18:17:42 -0400

    Implement collisions with ground

---
+++ Assets/Scripts/ContactWithGround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactWithGround : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Snowflake")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+
+        }
+    }
+}
--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,9 +6,9 @@ public class DestroyByContact : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
         if (other.tag == "Snowflake")
         {
+            Destroy(other.gameObject);
             transform.localScale += new Vector3(0.1F, 0.1F, 0);
         }
     }
