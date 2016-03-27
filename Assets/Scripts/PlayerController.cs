a490443d4f12f4519e52e19cdb2b3010b0d2994f
handrin <handrin@uwo.ca>
2016-03-27 00:13:55 -0400

    Snowflake switches sides when it gets to the edge of the screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class Boundary
-{
-    public float xMin, xMax;
-}
-
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Boundary boundary;
 
     void FixedUpdate()
     {
@@ -19,6 +12,5 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(moveHorizontal, 0.0f);
         GetComponent<Rigidbody2D>().velocity = movement * speed;
-        GetComponent<Rigidbody2D>().position = new Vector2(Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, boundary.xMin, boundary.xMax), transform.position.y);
     }
 }
---
+++ Assets/Scripts/SideSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
+public class SideSwitcher : MonoBehaviour {
+
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        if (GetComponent<Rigidbody2D>().position.x < boundary.xMin)
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(boundary.xMax, transform.position.y);
+        }
+        if (GetComponent<Rigidbody2D>().position.x > boundary.xMax)
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(boundary.xMin, transform.position.y);
+        }
+    }
+}
