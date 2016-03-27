e1e454658c82bcf625d9214e8313299f0d5f11b2
handrin <handrin@uwo.ca>
2016-03-27 14:45:43 -0400

    Player absorbs other snowflakes by contact

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+        if (other.tag == "Snowflake")
+        {
+            transform.localScale += new Vector3(0.1F, 0.1F, 0);
+        }
+    }
+}
