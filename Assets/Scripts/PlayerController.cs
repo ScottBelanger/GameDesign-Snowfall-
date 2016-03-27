1e77bb7e48501297062bd23f2d02e55eb3d71653
handrin <handrin@uwo.ca>
2016-03-26 22:42:40 -0400

    Add tilt controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //For controlling with the keyboard
+        //float moveHorizontal = Input.acceleration.x; //For tilt controls on phone
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+        GetComponent<Rigidbody2D>().position = new Vector2(Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, boundary.xMin, boundary.xMax), transform.position.y);
+    }
+}
