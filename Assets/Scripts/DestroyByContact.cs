476fa35a33f3481035625084443d0f462e463c69
Scott <sbelan@uwo.ca>
2016-04-03 20:22:31 -0400

    Implemented Score/Ground Measures
    -Implemented a score count
    -Implemented a ground distance measure

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DestroyByContact : MonoBehaviour
 {
+
+    public static float score;
+    Text text;
+
+    void Awake()
+    {
+        text = GameObject.FindWithTag("scoreText").GetComponent<Text>();
+        score = 0;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Snowflake")
         {
+            Debug.Log("Hit");
             Destroy(other.gameObject);
             transform.localScale += new Vector3(0.1F, 0.1F, 0);
+            score = score + 10;
+            text.text = "Score: " + score;
         }
     }
 }
---
+++ Assets/Scripts/Managers/DistanceToGround.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DistanceToGround : MonoBehaviour
+{
+    public static float distance;
+    Text text;
+
+    // Use this for initialization
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        distance = 0;
+    }
+
+    
+    void Update()
+    {
+        GameObject Snowflake = GameObject.FindWithTag("Player");
+        GameObject Ground = GameObject.FindWithTag("Ground");
+        distance = Vector3.Distance(Snowflake.transform.position, Ground.transform.position);
+        text.text = "Ground: " + distance;
+    }
+}
