7cb89269a03092eca185c8d373a29644754d7892
Scott <sbelan@uwo.ca>
2016-04-06 19:00:23 -0400

    Pause Option in Levels
    -Implemented a pause option

---
+++ Assets/Scripts/Managers/pauseMenuManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class pauseMenuManager : MonoBehaviour {
+
+    public GameObject menuPanel;
+	// Use this for initialization
+	void Start () {
+        menuPanel = GameObject.FindGameObjectWithTag("PauseMenu");
+        menuPanel.SetActive(false);
+
+	}
+	
+}
---
+++ Assets/Scripts/Managers/pauseScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseScript : MonoBehaviour {
+
+    public bool paused;
+	// Use this for initialization
+	void Start () {
+        paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void setPaused()
+    {
+        if (!paused)
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+        else if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
+        
+    }
+}
